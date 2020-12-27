c7a0474b925d31316b90d21d60c09d8d303985e3
unknown <jaredremsberg@gmail.com>
2020-12-26 21:08:05 -0500

    guh update

--- cogs/sbonks.py
+++ cogs/sbonks.py
@@ -42,6 +42,7 @@ class SbonkCommands(commands.Cog):
         change_symbol = "⬆️ " if change >= 0 else "⬇️ "
         change_symbol += f"{change:.2f}"
         change_percent = round(data["changePercent"]*100, 2)
+        is_guh = change_percent <= -5
 
         # generate and send sbonk embed
         sbonk_embed = discord.Embed(
@@ -50,49 +51,7 @@ class SbonkCommands(commands.Cog):
             color=discord.Color.green() if change >= 0 else discord.Color.red()
         )
         sbonk_embed.set_footer(text=data["latestTime"])
-        return sbonk_embed
-
-    # def fetch_stock(self, symbol):
-    #     """Fetch stock data and form into embed"""
-    #     symbol = symbol.upper()
-
-    #     # API requests from https://www.alphavantage.co/
-    #     response = requests.get(
-    #         f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=1min&apikey={self.api_key}")
-    #     live_data = response.json()
-    #     print(response.status_code)
-
-    #     response = requests.get(
-    #         f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={self.api_key}")
-    #     daily_data = response.json()
-    #     print(response.status_code)
-
-    #     print(live_data)
-    #     print(daily_data)
-
-    #     # important minute data
-    #     time = live_data["Meta Data"]["3. Last Refreshed"]
-    #     price = live_data["Time Series (1min)"][time]["4. close"]
-
-    #     # important daily data
-    #     last_close_date = daily_data["Meta Data"]["3. Last Refreshed"]
-    #     last_close_price = daily_data["Time Series (Daily)"][last_close_date]["4. close"]
-
-    #     # Convert data to suit embed
-    #     price_diff = round(float(price) - float(last_close_price), 2)
-    #     is_printing = price_diff >= 0  # if current price is higher than close
-    #     change = f"⬆️${price_diff}" if is_printing else f"⬇️${price_diff}"
-    #     percent_change = round(price_diff/float(last_close_price)*100, 2)
-    #     color = discord.Color.green() if is_printing else discord.Color.red()
-
-    #     # generate and send sbonk embed
-    #     sbonk_embed = discord.Embed(
-    #         title=f"{symbol}: ${round(float(price), 2)}",
-    #         description=f"{change} ({percent_change}%)",
-    #         color=color
-    #     )
-    #     sbonk_embed.set_footer(text=time)
-    #     return sbonk_embed
+        return sbonk_embed, is_guh
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -111,15 +70,19 @@ class SbonkCommands(commands.Cog):
         symbols = [s[1:-1] for s in prefixed_symbols]
 
         for symbol in symbols:
-            embed = self.fetch_stock_data(symbol)
+            embed, is_guh = self.fetch_stock_data(symbol)
 
             # Send weirdchamp if unrecognized symbol
             if not embed:
                 weirdchamp = bot.get_emoji(746570904032772238)
-                if weirdchamp:
-                    await message.channel.send(str(weirdchamp))
+                await message.channel.send(str(weirdchamp))
             else:
                 await message.channel.send(embed=embed)  # send sbonk embed
+
+            # Check if guh
+            if is_guh:
+                guh = bot.get_emoji(755546594446671963)
+                await message.channel.send(str(guh))
 
 
 def setup(bot):
