eebcc654b07692467c76a60209a5c582c3f772ce
unknown <sollyco@gmail.com>
2021-03-02 18:42:53 -0500

    added more poggers

--- common/cfg.py
+++ common/cfg.py
@@ -58,5 +58,10 @@ poggers_links = [
     "https://tenor.com/view/pog-poggers-pogchamp-pogchamps-pogger-gif-18414191",
     "https://tenor.com/view/poggers-anime-gif-19842013",
     "https://tenor.com/view/marnie-pokemon-anime-pog-pogchamp-gif-19642089",
-    "https://tenor.com/view/poggers-gif-19466277"
+    "https://tenor.com/view/poggers-gif-19466277",
+    "https://tenor.com/view/genshin-poggers-ningguang-cringe-anime-gif-18890744",
+    "https://tenor.com/view/genshin-lumine-poggers-gif-18795348",
+    "https://tenor.com/view/amber-poggers-genshin-impact-gif-18732929"
+
+
 ]
