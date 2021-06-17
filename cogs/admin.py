32fbd779e7338d411975fd6a33f76aadc822b5aa
Carlos <carlos_eduardo.ps@hotmail.com>
2021-06-17 12:55:33 -0400

    grow up

--- cogs/admin.py
+++ cogs/admin.py
@@ -13,13 +13,6 @@ class AdminCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.muted = set()
-        self.banned_gif_keywords = {
-            "showtime",  # eat a dick
-            "dog-crying",
-            "bruh",
-            "eyebrow-raise",
-            "incredibles"
-        }
 
     def cog_check(self, ctx):
         if not ctx.author.guild_permissions.administrator:
@@ -103,14 +96,6 @@ class AdminCommands(commands.Cog):
 
         if message.author.id in self.muted:
             await message.delete()
-
-        # ignore banned gifs
-        if message.content.startswith("https://tenor.com"):
-            for kw in self.banned_gif_keywords:
-                if kw in message.content:
-                    await message.delete()
-                    return await message.channel.send(utils.weirdchamp())
-
 
 def setup(bot):
     bot.add_cog(AdminCommands(bot))
