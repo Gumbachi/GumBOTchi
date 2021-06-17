cc54530a12c93d172170ddd61adcf1813957330c
Jared Remsberg <jaredremsberg@gmail.com>
2021-06-17 10:29:38 -0400

    add mafia file that will probably never be touched

---
+++ cogs/mafia.py
@@ -0,0 +1,24 @@
+"""Module with mafia related cog and commands."""
+
+from discord.ext import commands
+
+
+class MafiaCommands(commands.Cog):
+    """All mafia related commands."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='mafia')
+    async def begin_mafia_game(self, ctx):
+        """Start mafia game"""
+        await ctx.send("Not Implemented")
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        """Listen for reactions."""
+        pass
+
+
+def setup(bot):
+    bot.add_cog(MafiaCommands(bot))
