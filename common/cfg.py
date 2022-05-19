199e4ca3fc9f2c0ee11788bd8ee02c52eee73dc7
Gumbachi <jaredremsberg@gmail.com>
2022-05-19 14:28:18 -0400

    one more

--- common/cfg.py
+++ common/cfg.py
@@ -87,31 +87,6 @@ poggers_activation_phrases = {
     "pogflip"
 }
 
-# poggers_links = [
-#     "https://tenor.com/view/anime-poggers-anime-poggers-anime-gif-18290521",
-#     "https://tenor.com/view/anime-poggers-anime-poggers-anime-gif-18290518",
-#     "https://tenor.com/view/poggers-gif-18334778",
-#     "https://tenor.com/view/poggers-kiss-anime-kiss-honey-gif-18097318",
-#     "https://tenor.com/view/anime-poggers-sound-of-poggers-poggers-anime-yuru-yuri-gif-18409324",
-#     "https://tenor.com/view/poggers-anime-girls-kissing-pog-gif-18050577",
-#     "https://tenor.com/view/sound-of-poggers-poggers-anime-anime-poggers-charr-gif-18348699",
-#     "https://tenor.com/view/poggers-pog-pogchamp-anime-illya-gif-18054845",
-#     "https://tenor.com/view/poggers-based-lesbian-anime-gif-18956362",
-#     "https://tenor.com/view/poggers-gif-18334779",
-#     "https://tenor.com/view/poggers-anime-girls-mako-anime-girls-kiss-kiss-gif-17206802",
-#     "https://tenor.com/view/anime-poggers-anime-poggers-anime-gif-18290513",
-#     "https://tenor.com/view/poggers-anime-anime-poggers-poggers-anime-gif-18386348",
-#     "https://tenor.com/view/poggers-anime-kiss-girl-gif-18969493",
-#     "https://tenor.com/view/anime-poggers-anime-poggers-anime-gif-18290523",
-#     "https://tenor.com/view/pog-poggers-pogchamp-pogchamps-pogger-gif-18414191",
-#     "https://tenor.com/view/poggers-anime-gif-19842013",
-#     "https://tenor.com/view/marnie-pokemon-anime-pog-pogchamp-gif-19642089",
-#     "https://tenor.com/view/poggers-gif-19466277",
-#     "https://tenor.com/view/genshin-poggers-ningguang-cringe-anime-gif-18890744",
-#     "https://tenor.com/view/genshin-lumine-poggers-gif-18795348",
-#     "https://tenor.com/view/amber-poggers-genshin-impact-gif-18732929",
-# ]
-
 spam_words = [
     'Smartphones', 'iPhone', 'Samsung', 'LG', 'Android', 'Laptops',
     'Video Games', 'Drones', 'Speakers', 'Cameras',
